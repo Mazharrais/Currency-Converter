9e83b31ade1de82ecfa2020ac6181c5bc7a791ba
Mazharrais <mazharmajo72@gmail.com>
2023-12-23 17:52:35 +0500

    add changes

--- app.js
+++ app.js
@@ -25,6 +25,25 @@ for(let select of dropdowns){
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
+    if(select.name === "from" && currCode === "USD"){
+      newOption.selected = "selected";
+    } else if(select.name === "to" && currCode === "PKR"){
+      newOption.selected = "selected";
+    }
     select.append(newOption);
   }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  })
+}
+
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc
+
+  console.log(img.src);
 }

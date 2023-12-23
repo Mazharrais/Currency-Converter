70cb7c2b3b297e77b5f8bf4dad6fd662fc2ccba9
Mazharrais <mazharmajo72@gmail.com>
2023-12-23 17:32:01 +0500

    add changes

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const Base_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 
-for(code in countryList){
-  console.log(code);
+
+for(let select of dropdowns){
+  
+  for(currCode in countryList){
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    select.append(newOption);
+  }
 }

fc5c8f2c53d409ba24179efacc1fa0e6d20dfd10
Mazharrais <mazharmajo72@gmail.com>
2023-12-23 15:00:00 +0500

    add changes

---
+++ app.js
@@ -0,0 +1,14 @@
+// const URL = "https://cat-fact.herokuapp.com/facts";
+// const factPara = document.querySelector("#fact");
+// const button = document.querySelector("#btn");
+
+
+
+// const getFacts = async() => {
+//   console.log("getting data...");
+//   let response = await fetch(URL);
+//   console.log(response);  //json formate
+//   let data = await response.json();
+//   factPara.innerHTML = data[1].text
+// }
+// button.addEventListener("click", getFacts);
